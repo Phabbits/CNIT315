aecefacf8bc706a163a82b40f985ddb86dc221cf
Kylen416 <81328018+Kylen416@users.noreply.github.com>
2021-04-02 13:45:11 -0400

    4/2 Meeting

--- Test.C
+++ Test.C
@@ -3,3 +3,70 @@
 Hello
 
 */
+
+int main(){ 
+
+    //initialize game 
+
+    //initialize equipment structures
+    //initailize encrypt structures
+    //initialize 2d of attack effects (encrypt)
+    //create an array storing each equipment structure
+
+    //order attacks for game rounds 
+
+    //request player name 
+
+     
+
+    //initialize player 
+
+    //create an equipment array of amount of equipment
+    //create a dymanic list from pointers to encrypt
+
+    //set startup equipment, encryption method, credits 
+
+     
+
+    while(player has options and more rounds exists){ 
+
+        //output player status (if statments)
+        //current credit
+        //current equipment
+        //pulls from info from index amount table referencing index struct table
+        //current encrypt
+        //iderate and print through current encryption 
+        
+        //recieve credits 
+        //use equation to figure out how many credit user recieves 
+        //tell user how credits recived and what encrypt method was used 
+
+        //present attack 
+        //print attack details and affects of attack 
+        //edit encrypt structure effectiveness 
+
+        //spend credits interactive menu 
+
+        //display avaible equiptment and encrypt options 
+        //as well how many credits and what the current inventory is
+        
+        //buying equipment 
+        //increase amount in index amount if things are bought
+        //decrease credits if things are bought
+
+        //buying encrypt
+        //add pointer to encrypt linked list 
+        //decrease credits if things are brought
+
+        //selling enquipment
+        //decrease amount in index amount if things are sold 
+        //increase credit if things are sold
+
+
+    } 
+
+    //save player score to leaderboard (prototype 2) 
+
+} 
+
+     
