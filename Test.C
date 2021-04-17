242f4ce7d28297aed7878b1e84cf0c058aaf0d67
memety1 <81328375+memety1@users.noreply.github.com>
2021-04-17 18:37:22 -0400

    Update Test.C
    Co-authored-by: Phabbits <gabrieljfrey@gmail.com>

--- Test.C
+++ Test.C
@@ -253,7 +253,7 @@ int main(){
         scanf("\n%d", &equipSellSelect);
 
         while (equipSellSelect < NUM_EQUIPMENT){ // while the user is selecting something to sell and not choseing to pass
-            printf("You have selected: %s", equipmentStock[0].name); // not sure if that should be equipmentStock or not
+            printf("You have selected: %s\n", equipmentStock[0].name); // not sure if that should be equipmentStock or not
             printf("\n How many would you like to sell? ");
             scanf("\n%d", &equipSellAmount);
             equipSellTransaction = equipmentStock[0][2] * equipSellAmount; // not sure if that should be equipmentStock or not
