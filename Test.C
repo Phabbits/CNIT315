340213b4fd781a521e6bac8eddd6bc56e9c0cf81
Kylen416 <81328018+Kylen416@users.noreply.github.com>
2021-04-29 14:40:16 -0400

    Update Test.C
    Co-authored-by: Phabbits <gabrieljfrey@gmail.com>

--- Test.C
+++ Test.C
@@ -601,7 +601,7 @@ void SellingStore(struct Player *playerPtr, struct Equipment equipmentStock[NUM_
             printf("Equipment that you own:\n");
             printf(" # | Equipment Name                       | Cost  | Message Amount | Inventory |\n");
             printf("---|--------------------------------------|-------|----------------|-----------|\n");
-            for(int i=0; i<NUM_EQUIPMENT; i++){
+            for (int i=0; i<NUM_EQUIPMENT; i++){
                 printf(" %d | %36s | %5d | %14d | %9d |\n", i + 1, equipmentStock[i].name, equipmentStock[i].cost, equipmentStock[i].messageAmount, playerPtr->equipmentInventory[i]);
             }
             printf("Please select the equipment you would like to sell or select %d to pass: ", NUM_EQUIPMENT + 1);
